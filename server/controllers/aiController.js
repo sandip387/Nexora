35a5d4785140601ca7881cc81a1ff1745691de82
Sandip <sandipshrestha387@gmail.com>
2025-09-03 08:53:18 +0545

    feat: Integrate Clerk authentication in App component token generation  and add Cloudinary configuration; implement AI blog title generation with error handling FIX: error in api calling for article generation

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,9 +1,10 @@
 import OpenAI from "openai";
 import sql from "./../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 
 const AI = new OpenAI({
-  apiKey: process.env.GEMINI_API_KEY,
+  apiKey: process.env.NEXORA_GEMINI_API_KEY,
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
 
@@ -43,9 +44,59 @@ export const generateArticle = async (req, res) => {
         privateMetadata: { free_usage: free_usage + 1 },
       });
     }
-    res.json({ success: true }, content);
+    res.json({ success: true, content });
   } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
+    res.status(500).json({
+      success: false,
+      message: "Failed to generate content.",
+      error: error.message,
+    });
   }
 };
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "Limit reached. Upgrade to continue.",
+      });
+    }
+
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: { free_usage: free_usage + 1 },
+      });
+    }
+    res.json({ success: true, content });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to generate content.",
+      error: error.message,
+    });
+  }
+};
+
