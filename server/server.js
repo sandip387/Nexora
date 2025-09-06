7f1b33f2d8a000db117a3eb6f73cb277c6eb9292
Sandip <sandipshrestha387@gmail.com>
2025-09-06 20:59:23 +0545

    feat: Add user creation routes and controller methods for fetching and liking creations

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 import aiRouter from "./routes/aiRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 app.use(requireAuth());
 
 app.use("/api/ai", aiRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
